440414fbfe5abe24fd18d85910d205651a844696
jendiamond <thejendiamond@gmail.com>
2021-09-27 17:30:51 -0700

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header>
+      <nuxt-link to="/">HOME</nuxt-link>
+    </header>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family:
+    'Helvetica Neue';
+    font-size: 16px;
+    color: #999;
+}
+a {
+  color: skyblue;
+  text-decoration: none;
+}
+</style>
---
+++ pages/home/_id.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+    <div style="display: flex">
+        <img v-for="image in home.images" :key="image" :src="image" width="200"/>
+    </div>
+    {{ home.title }}<br>
+    ${{ home.pricePerNight }} / night<br>
+    <img src="/images/marker.svg" alt="map-marker" width="20">{{ home.location.address }}
+    {{ home.location.city }}
+    {{ home.location.state }}
+    {{ home.location.country }}<br>
+    <img src="/images/star.svg" alt="star" width="20">{{ home.reviewValue }}<br>
+    {{ home.guests }} guests,
+    {{ home.bedrooms }} rooms,
+    {{ home.beds }} beds,
+    {{ home.bathrooms }} bath
+    </div>
+</template>
+
+<script>
+import homes from '~/data/homes'
+
+export default {
+    head(){
+        return {
+            title: this.home.title
+        }
+    },
+    data(){
+        return {
+            home: {}
+        }
+    },
+    created() {
+        const home = homes.find((home) =>home.objectID == this.$route.params.id)
+        this.home = home
+    },
+}
+</script>
